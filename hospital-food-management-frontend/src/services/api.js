b1a58d521babf039130f06c0cef6d0f8766ff0af
AvishkarMandlik <mandlikavi121@gmail.com>
2025-01-12 00:10:01 +0530

    backend added

--- hospital-food-management-frontend/src/services/api.js
+++ hospital-food-management-frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 // Create an Axios instance to interact with the backend API
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Make sure the backend is running on this URL
+  // baseURL: 'http://localhost:5000/api', // Make sure the backend is running on this URL
+  baseURL: 'https://hospital-food-management-backend-itv7.onrender.com', // Make sure the backend is running on this URL
   headers: {
     'Content-Type': 'application/json',
   },
