0daec13fb92e81b530c211ad174ff1fd9dfb245c
AvishkarMandlik <mandlikavi121@gmail.com>
2025-01-13 13:14:41 +0530

    update config

--- Server/config/database.js
+++ Server/config/database.js
@@ -2,22 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const mongoUri = process.env.MONGO_URI;
-    
-    // Check if MONGO_URI is defined
-    if (!mongoUri) {
-      throw new Error('MONGO_URI is not defined in the environment variables.');
-    }
-
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,    // Ensure backward compatibility
-      useUnifiedTopology: true // Recommended for MongoDB driver >= 3.2
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-
     console.log('MongoDB connected...');
   } catch (err) {
-    console.error(`Error: ${err.message}`);
-    process.exit(1); // Exit the process with failure
+    console.error(err.message);
+    process.exit(1);
   }
 };
 
