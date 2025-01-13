f2d77ea142c703e721c453f7e011dbcfef4f3fe8
AvishkarMandlik <mandlikavi121@gmail.com>
2025-01-13 14:20:24 +0530

    env added

--- Client/src/services/api.js
+++ Client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' 
-    ? 'https://hospital-food-management-59px.onrender.com/api' 
+  baseURL: process.env.REACT_ENV === 'production' 
+    ? 'https://hospital-food-management-backend-qyq0.onrender.com/api' 
     : 'http://localhost:5000/api', // Local development URL
   headers: {
     'Content-Type': 'application/json',
